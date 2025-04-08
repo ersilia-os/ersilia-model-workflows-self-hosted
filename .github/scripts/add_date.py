991b980cd9847d9997a66d88deb3d346d47efd1e
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-04-08 22:08:34 +0200

    Add files via upload

---
+++ .github/scripts/add_date.py
@@ -0,0 +1,21 @@
+import sys
+import json
+import yaml
+
+file_name = sys.argv[1]
+date_str = sys.argv[2]
+
+if file_name.endswith(".json"):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    data['Incorporation Date'] = date_str
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4)
+elif file_name.endswith(".yaml") or file_name.endswith(".yml"):
+    with open(file_name, 'r') as f:
+        data = yaml.safe_load(f)
+    data['Incorporation Date'] = date_str
+    with open(file_name, 'w') as f:
+        yaml.dump(data, f, sort_keys=False)
+else:
+    raise ValueError("Unsupported file format. Please provide a .json or .yaml file.")
