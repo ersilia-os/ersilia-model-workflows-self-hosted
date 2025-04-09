b3bbfa4ba0cf65a9ab136769e9b7c10a775a7502
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-04-09 18:15:23 +0200

    Update add_date.py

--- .github/scripts/add_date.py
+++ .github/scripts/add_date.py
@@ -1,6 +1,6 @@
 import sys
 import json
-import yaml
+from ruamel.yaml import YAML
 
 file_name = sys.argv[1]
 date_str = sys.argv[2]
@@ -12,10 +12,11 @@ if file_name.endswith(".json"):
     with open(file_name, 'w') as f:
         json.dump(data, f, indent=4)
 elif file_name.endswith(".yaml") or file_name.endswith(".yml"):
+    yaml = YAML()
     with open(file_name, 'r') as f:
-        data = yaml.safe_load(f)
+        data = yaml.load(f)
     data['Incorporation Date'] = date_str
     with open(file_name, 'w') as f:
-        yaml.dump(data, f, sort_keys=False, default_flow_style=False)
+        yaml.dump(data, f)
 else:
     raise ValueError("Unsupported file format. Please provide a .json or .yaml file.")
